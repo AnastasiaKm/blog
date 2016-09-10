3c452877d417c721726e075c9232dd850907faee
Anastasia Michail <a.karamichailidou@gmail.com>
2016-09-10 11:33:32 +0300

    Trying to fix a bug in the blog.index view

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -17,20 +17,20 @@
       </div> <!-- col -->
     {{-- </div> <!-- row --> --}}
 
-    @foreach($posts as $post)
       {{-- <div class="row"> --}}
         <div class="col-sm-6 col-sm-offset-2">
-          <h2>{{ $post->title }}</h2>
-          <h5>Published: {{ date('M j, Y', strtotime($post->created_at)) }}</h5>
-          <p>
-            {{ substr(strip_tags($post->body), 0, 250) }}
-            {{ strlen(strip_tags($post->body)) > 250 ? "..." : "" }}
-          </p>
-          <a href="{{ route('blog.single', $post->slug) }}" class="btn btn-primary">Read More</a>
-          <hr>
+          @foreach($posts as $post)
+            <h2>{{ $post->title }}</h2>
+            <h5>Published: {{ date('M j, Y', strtotime($post->created_at)) }}</h5>
+            <p>
+              {{ substr(strip_tags($post->body), 0, 250) }}
+              {{ strlen(strip_tags($post->body)) > 250 ? "..." : "" }}
+            </p>
+            <a href="{{ route('blog.single', $post->slug) }}" class="btn btn-primary">Read More</a>
+            <hr>
+          @endforeach
         </div> <!-- col -->
 
-    @endforeach
     </div> <!-- row -->
   {{-- </div> <!-- container --> --}}
 
